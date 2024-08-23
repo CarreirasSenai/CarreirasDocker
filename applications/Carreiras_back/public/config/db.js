8ce8e1e95a07b0ec31fff3cbcc0c478486d908a8
Thiago <mauesckt@gmail.com>
2024-08-22 22:05:53 -0300

    FUN-CIO-NOU

--- applications/Carreiras_back/public/config/db.js
+++ applications/Carreiras_back/public/config/db.js
@@ -1,18 +1,23 @@
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-  host: 'carreiras-mysql-1',
-  user: 'root',
-  password: 'root',
-  database: 'carreiras'
+// Criação de uma conexão com o banco de dados MySQL
+const db = mysql.createConnection({
+  host: process.env.DB_HOST || 'localhost', // Host do banco de dados
+  user: process.env.DB_USER || 'root', // Nome de usuário do banco de dados
+  password: process.env.DB_PASS || 'root', // Senha do banco de dados
+  database: process.env.DB_NAME || 'carreiras', // Nome do banco de dados
+  charset: 'utf8mb4' // Especifique a codificação UTF-8
 });
 
-connection.connect((err) => {
+// Conecta-se ao banco de dados
+db.connect((err) => {
   if (err) {
+    // Se houver um erro ao conectar, exibe o erro no console
     console.error('Erro ao conectar ao banco de dados:', err);
     return;
   }
-  console.log('Conectado ao banco de dados!');
+  // Se a conexão for bem-sucedida, exibe uma mensagem de sucesso no console
+  console.log('Conexão com o banco de dados estabelecida');
 });
 
-module.exports = connection;
+module.exports = db;
